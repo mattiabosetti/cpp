3bd4f3096f1549b53d9700f8c158b507f8f3ed84
mattiabosetti <mattbos.2007@gmail.com>
2023-10-18 19:53:19 +0200

    .

--- compiti 3a/Bosetti_4.7.cpp
+++ compiti 3a/Bosetti_4.7.cpp
@@ -6,7 +6,6 @@ int main(){
     SetConsoleOutputCP(CP_UTF8);
     int i=0, numeri[100], n=0;
     double media, somma = 0;
-
     do{
         cout << "Inserisci il " << i+1 << "° numero: ";
         cin >> numeri[i];
@@ -15,21 +14,14 @@ int main(){
             cin >> numeri[0];
         }
         i++;
-
     } while(numeri[i] != 0);
     n = i-1;
     i = 0;
-
     while(i <= n) {
         somma += numeri[i];
         i++;
     }
-
     media = somma/n;
-
     cout << "La media dei numeri inseriti é: " << media;
-
-
-
     return 0;
 }
