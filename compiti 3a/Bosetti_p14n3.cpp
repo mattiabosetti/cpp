15b6c12ac284e63580ae3fe370eecd2cc7612ab1
mattiabosetti <mattbos.2007@gmail.com>
2023-10-31 18:13:02 +0100

    aggiunto Bosetti_p14n3

---
+++ compiti 3a/Bosetti_p14n3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <windows.h>
+
+int x;
+void somma();
+void moltiplica();
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+
+    x = 9;
+    moltiplica();
+    
+    return 0;
+}
+
+void moltiplica() {
+    double x;
+    x = 2;
+    somma();
+}
+void somma() {
+    int Voc;
+    double x, y, z;
+    x = 2;
+}
