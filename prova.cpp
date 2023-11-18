6d79d389736d6825c4e4d8dff369768feb8506db
Mattia Bosetti <mattia@debian.debian.com>
2023-11-18 12:26:29 +0100

    .

---
+++ prova.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vettore;
+
+    vettore = {1,2,3,4,5};
+
+    cout << vettore[2] << endl;
+
+
+    return 0;
+}
